2fed10d34f114b18b25e54ff585763f8c550fbea
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-26 18:09:59 +0300

    Adding .gitingore and All Problems Description and Solutions to P01 and 02 to Suject 01.Inheritance-Lab

---
+++ 01.Inheritance/Inheritance-Lab/P02.MultipleInheritance/Puppy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public  class Puppy : Dog
+    {
+        public void Weep()
+            => Console.WriteLine("weeping…");
+    }
+}
