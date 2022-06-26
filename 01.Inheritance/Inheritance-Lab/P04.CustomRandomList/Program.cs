0ebc5311bde76a977438649363929dc60acbddc3
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-26 23:11:55 +0300

    Adding Solution to P04 and P05

---
+++ 01.Inheritance/Inheritance-Lab/P04.CustomRandomList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("Pesho");
+            list.Add("Gosho");
+            list.Add("Penko");
+            list.Add("Vanko");
+
+            
+            Console.WriteLine(list.RandomString() + " has been removed from the list.");
+
+            Console.WriteLine();
+            Console.WriteLine("List of people:");
+            foreach (var person in list)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
---
+++ 01.Inheritance/Inheritance-Lab/P05.GustomStack/StackOfStrings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+            => this.Count == 0;
+
+
+        public Stack<string> AddRange(IEnumerable<string> elementsToAdd)
+        {
+            foreach (var element in elementsToAdd)
+            {
+                this.Push(element);
+            }
+
+            return this;
+        }
+    }
+}
