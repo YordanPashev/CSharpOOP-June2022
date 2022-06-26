8cca853117a7bd6c5cc8e476087b3709f747a8a7
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-27 02:35:53 +0300

    Adding All Problmes Descripton and Solution to P01 and 02 to Inheritance-Exercise

---
+++ 01.Inheritance/Inheritance - Exercise/P01.Person/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private int age;
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public virtual int Age
+        { 
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A person age can't be negative.");
+                }
+
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        =>$"Name: {Name}, Age: {Age}";
+    }
+}
