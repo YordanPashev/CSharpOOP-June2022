15fdc0ea8beeac5b4c2d5641446df6e5fda88b63
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-02 12:52:00 +0300

    Adding Subject Polymorphism, All Problems Description to Lab and Solution to P01

---
+++ 04.Polymorphism/Polymorphism - Lab/P01.MathOperations/MathOperations.cs
@@ -0,0 +1,15 @@
+﻿namespace Operations
+{
+    public class MathOperations
+    {
+
+        public int Add(int x, int y)
+            => x + y;
+
+        public double Add(double x, double y, double c)
+           => x + y + c;
+
+        public decimal Add(decimal x, decimal y, decimal c)
+           => x + y + c;
+    }
+}
