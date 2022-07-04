d1dfd891b9a8b90800906ac4359074fbb8abe9d3
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-04 14:24:48 +0300

    Refactoring Solution to P04.WildFarm

---
+++ 04.Polymorphism/Polymorphism - Exercise/P04.WildFarm/Factories/AnimalFactory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using WildFarm.Models.Animals;
+using WildFarm.Models.Animals.Birds;
+using WildFarm.Models.Animals.Mammals;
+using WildFarm.Models.Animals.Mammals.Felines;
+
+namespace WildFarm.Factories
+{
+    public static class AnimalFactory
+    {
+        public static Animal CreateAnimal(string[] animalInfo)
+        {
+            string type = animalInfo[0];
+            string name = animalInfo[1];
+            double weight = double.Parse(animalInfo[2]);
+            Animal animal = null;
+
+            if (type == "Owl")
+            {
+                double wingSize = double.Parse(animalInfo[3]);
+                animal=  new Owl(name, weight, wingSize);
+            }
+
+            else if (type == "Hen")
+            {
+                double wingSize = double.Parse(animalInfo[3]);
+                animal = new Hen(name, weight, wingSize);
+            }
+
+            else if (type == "Mouse")
+            {
+                string livingRegion = animalInfo[3];
+                animal = new Mouse(name, weight, livingRegion);
+            }
+
+            else if (type == "Dog")
+            {
+                string livingRegion = animalInfo[3];
+                return new Dog(name, weight, livingRegion);
+            }
+
+            else if (type == "Cat")
+            {
+                string livingRegion = animalInfo[3];
+                string breed = animalInfo[4];
+                animal = new Cat(name, weight, livingRegion, breed);
+            }
+
+            else if (type == "Tiger")
+            {
+                string livingRegion = animalInfo[3];
+                string breed = animalInfo[4];
+                animal = new Tiger(name, weight, livingRegion, breed);
+            }
+
+            return animal;
+        }
+
+    }
+}
---
+++ 04.Polymorphism/Polymorphism - Exercise/P04.WildFarm/Factories/FoodFactory.cs
@@ -0,0 +1,36 @@
+﻿using WildFarm.Models.Foods;
+
+namespace WildFarm.Factories
+{
+    public static class FoodFactory
+    {
+        public static Food CreateFood(string[] foodInfo)
+        {
+            string type = foodInfo[0];
+            int qunatity = int.Parse(foodInfo[1]);
+            Food food = null;
+
+            if (type == "Vegetable")
+            {
+                food = new Vegetable(qunatity);
+            }
+
+            else if (type == "Fruit")
+            {
+                food = new Fruit(qunatity);
+            }
+
+            else if (type == "Meat")
+            {
+                food = new Meat(qunatity);
+            }
+
+            else if (type == "Seeds")
+            {
+                food = new Seeds(qunatity);
+            }
+
+            return food;
+        }
+    }
+}
