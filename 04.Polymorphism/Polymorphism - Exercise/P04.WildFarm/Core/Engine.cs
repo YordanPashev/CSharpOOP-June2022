1b1076ddbb9d771cd975688b65f6c9ed96c8b0a4
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-04 14:25:50 +0300

    Refactoring the Solution to P04.WildFarm

--- 04.Polymorphism/Polymorphism - Exercise/P04.WildFarm/Core/Engine.cs
+++ 04.Polymorphism/Polymorphism - Exercise/P04.WildFarm/Core/Engine.cs
@@ -5,6 +5,7 @@ using WildFarm.Models.Animals.Mammals;
 using WildFarm.Models.Foods;
 using WildFarm.Models.Animals;
 using WildFarm.Models.Animals.Mammals.Felines;
+using WildFarm.Factories;
 
 namespace WildFarm.Core
 {
@@ -28,8 +29,8 @@ namespace WildFarm.Core
                 string[] foodInfo = Console.ReadLine()
                     .Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-                Animal animal = CreateAnimal(animalInfo);
-                Food food = CreateFood(foodInfo);
+                Animal animal = AnimalFactory.CreateAnimal(animalInfo);
+                Food food = FoodFactory.CreateFood(foodInfo);
                 animals.Add(animal);
 
                 Console.WriteLine(animal.ProduceSound());
@@ -39,83 +40,7 @@ namespace WildFarm.Core
             DisplayAllAnimals(animals);
         }
 
-        private Animal CreateAnimal(string[] animalInfo)
-        {
-            string type = animalInfo[0];
-            string name = animalInfo[1];
-            double weight = double.Parse(animalInfo[2]);
-
-            if (type == "Owl")
-            {
-                double wingSize = double.Parse(animalInfo[3]);
-                return new Owl (name, weight, wingSize); 
-            }
-
-            else if (type == "Hen")
-            {
-                double wingSize = double.Parse(animalInfo[3]);
-                return new Hen(name, weight, wingSize);
-            }
-
-            else if (type == "Mouse")
-            {
-                string livingRegion = animalInfo[3];
-                return new Mouse(name, weight, livingRegion);
-            }
-
-            else if (type == "Dog")
-            {
-                string livingRegion = animalInfo[3];
-                return new Dog(name, weight, livingRegion);
-            }
-
-            else if (type == "Cat")
-            {
-                string livingRegion = animalInfo[3];
-                string breed = animalInfo[4];
-                return new Cat(name, weight, livingRegion, breed);
-            }
-
-            else if (type == "Tiger")
-            {
-                string livingRegion = animalInfo[3];
-                string breed = animalInfo[4];
-                return new Tiger(name, weight, livingRegion, breed);
-            }
-
-            return null;
-        }
-
-
-        private Food CreateFood(string[] foodInfo)
-        {
-            string type = foodInfo[0];
-            int qunatity = int.Parse(foodInfo[1]);
-
-            if (type == "Vegetable")
-            {
-                return new Vegetable(qunatity);
-            }
-
-            else if (type == "Fruit")
-            {
-                return new Fruit(qunatity);
-            }
-
-            else if (type == "Meat")
-            {
-                return new Meat(qunatity);
-            }
-
-            else if (type == "Seeds")
-            {
-                return new Seeds(qunatity);
-            }
-
-            return null;
-        }
-
-        private void TryToFeedTheAnimal(Animal animal, Food food)
+        public static void TryToFeedTheAnimal(Animal animal, Food food)
         {
 
             if (animal.GetType().Name == "Hen")
@@ -131,14 +56,14 @@ namespace WildFarm.Core
                 animal.FoodEaten += food.Quantity;
             }
 
-            else if (animal.GetType().Name == "Cat" && 
+            else if (animal.GetType().Name == "Cat" &&
                     (food.GetType().Name == "Meat" || food.GetType().Name == "Vegetable"))
             {
                 animal.Weight += food.Quantity * 0.30;
                 animal.FoodEaten += food.Quantity;
             }
 
-            
+
             else if (animal.GetType().Name == "Tiger" && food.GetType().Name == "Meat")
             {
                 animal.Weight += food.Quantity * 1.00;
