6cfa912f1da0e5680738b7c5492e8eb50291cb12
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-04 19:34:35 +0300

    Adding All problems Descriptions to Subject Exceptions and Error Handling and Solution to P01.SquareRoot

---
+++ 05.Exceptions and Error Handling/P01.SquareRoot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                Console.WriteLine(Math.Sqrt(number));
+            }
+
+            catch (InvalidOperationException)
+            { 
+                Console.WriteLine("Invalid number.");
+            }
+
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
