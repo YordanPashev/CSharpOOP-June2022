4f2749d52289ebb67ec4e491573ef15c60925609
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-05 11:39:09 +0300

    Adding Solution to P03.Cards

---
+++ 05.Exceptions and Error Handling/P03.Cards/Card.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.Cards
+{
+    public class Card
+    {
+        private string face;
+        private string suit;
+
+        public Card(string face, string suit)
+        {
+            Face = face;
+            Suit = suit;
+        }
+
+        public string Face
+        {
+            get => face;
+            private set
+            {
+                if (!isFaceValid(value))
+                {
+                    throw new InvalidOperationException();
+                }
+
+                face = value;
+            }
+        }
+
+        public string Suit
+        {
+            get => suit;
+            private set
+            {
+                if (!isSuiteValid(value))
+                {
+                    throw new InvalidOperationException();
+                }
+
+                value = GetSuitSymbol(value);
+                suit = value;
+            }
+        }
+
+        private bool isFaceValid(string input)
+        {
+            List<string> validFaces = new List<string>
+            {
+                "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
+            };
+
+            if (validFaces.Contains(input))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool isSuiteValid(string input)
+        {
+            List<string> validSuits = new List<string>
+            {
+                        "S", "H", "D", "C"
+            };
+
+            if (validSuits.Contains(input))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private string GetSuitSymbol(string suiteLetter)
+        {
+            string suitSymbol = string.Empty;
+
+            if (suiteLetter == "S")
+            {
+                suitSymbol = "\u2660";
+            }
+
+            else if (suiteLetter == "H")
+            {
+                suitSymbol = "\u2665";
+            }
+
+            else if (suiteLetter == "D")
+            {
+                suitSymbol = "\u2666";
+            }
+
+            else if (suiteLetter == "C")
+            {
+                suitSymbol = "\u2663";
+            }
+
+            return suitSymbol;
+        }
+
+        public override string ToString()
+            => $"[{this.Face}{this.Suit}]";
+    }
+}
---
+++ 05.Exceptions and Error Handling/P03.Cards/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.Cards
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Card> cards = new List<Card>();
+
+            string[] inputLine = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string card in inputLine)
+            {
+                try
+                {
+                    string[] cardInfo = card.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    string face = cardInfo[0];
+                    string suit = cardInfo[1];
+
+                    Card cardToAdd = new Card(face, suit);
+
+                    cards.Add(cardToAdd);
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("Invalid card!");
+                    continue;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", cards));
+        }
+    }
+}
+
+
