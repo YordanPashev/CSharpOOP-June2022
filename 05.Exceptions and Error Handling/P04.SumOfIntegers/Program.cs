da9af269b357a8dc5f692b7649efadb3655b65d5
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-05 12:30:00 +0300

    Adding Solution to P04.SumOfIntegers

---
+++ 05.Exceptions and Error Handling/P04.SumOfIntegers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+
+namespace P04.SumOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine().Split(" ");
+            BigInteger bigInt = new BigInteger();
+            int sum = 0;
+
+            foreach (string element in elements)
+            {
+                try
+                {
+                    if (BigInteger.TryParse(element, out bigInt))
+                    {
+                        if (bigInt > int.MaxValue || bigInt < int.MinValue)
+                        {
+                            throw new OverflowException($"The element '{element}' is out of range!");
+                        }
+
+                        else
+                        {
+                            sum += int.Parse(element);
+                        }
+                    }
+
+                    else
+                    {
+                        throw new FormatException($"The element '{element}' is in wrong format!");
+                    }
+                }
+
+                catch (OverflowException overFlowEx)
+                {
+                    Console.WriteLine(overFlowEx.Message);
+                }
+
+                catch(FormatException formatEx) 
+                {
+                    Console.WriteLine(formatEx.Message);
+                }
+
+                finally
+                {
+                    Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
