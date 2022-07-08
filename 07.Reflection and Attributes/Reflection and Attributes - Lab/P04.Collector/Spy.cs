f6774b6c508b04e4e0bc6a413f2a52c668702a3a
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-09 00:44:37 +0300

    Adding Solution to P04.Collector

---
+++ 07.Reflection and Attributes/Reflection and Attributes - Lab/P04.Collector/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string CollectGettersAndSetters(string className)
+        {
+            StringBuilder result = new StringBuilder();
+            Type type = Type.GetType("Stealer." + className);
+            MethodInfo[] nonPublicMethods = type.GetMethods((BindingFlags)60);
+
+            foreach (MethodInfo method in nonPublicMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                result.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (MethodInfo method in nonPublicMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                result.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
