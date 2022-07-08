5e8aba4ee4566555e881444d35e2b4d3d2765de8
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-08 11:52:42 +0300

    Removing empty spaces in xmlLayout Format

--- 06.SOLID/SOLID-Exercise/Logger/Layouts/XmlLayout.cs
+++ 06.SOLID/SOLID-Exercise/Logger/Layouts/XmlLayout.cs
@@ -6,8 +6,8 @@ namespace SolidExerciseLogger.Layouts
     {
         private static string xmlLayoutFormat = $"<log>{Environment.NewLine}" +
                                                 "   <date>{0}</date>" + Environment.NewLine +
-                                                "   <level>{1}</level> " + Environment.NewLine +
-                                                "   <message>{2}</message> " + Environment.NewLine +
+                                                "   <level>{1}</level>" + Environment.NewLine +
+                                                "   <message>{2}</message>" + Environment.NewLine +
                                                 $"</log>";
 
         public XmlLayout() : base(xmlLayoutFormat) { }
