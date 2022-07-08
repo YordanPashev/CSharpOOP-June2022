1951d2a3899a4d8876b6780cf548a9bc23dcc4bf
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-09 00:04:55 +0300

    Adding Solution to P.03MissionPrivateImpossible

---
+++ 07.Reflection and Attributes/Reflection and Attributes - Lab/P03.MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string RevealPrivateMethods(string className)
+        {
+            StringBuilder result = new StringBuilder();
+            Type type = Type.GetType("Stealer." + className);
+            MethodInfo[] nonPublicMethods = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            result.AppendLine($"All Private Methods of Class: {type.Name}");
+            result.AppendLine($"Base Class: {type.BaseType.Name}");
+
+            foreach (MethodInfo method in nonPublicMethods)
+            {
+                result.AppendLine($"{method.Name}");
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
