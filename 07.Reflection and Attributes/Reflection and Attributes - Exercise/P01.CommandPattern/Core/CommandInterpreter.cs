ea5a8d953d2f5753e26cb137a1dcb9f2a22877aa
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-09 17:06:23 +0300

    Adding All Problems DEscription and Solution to P01.CommandPattern

---
+++ 07.Reflection and Attributes/Reflection and Attributes - Exercise/P01.CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CommandPattern.Commands;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Models
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] input = args.Split(' ');
+            string cmd = input[0];
+            string[] value = input[1..];
+
+            Type commandType = Assembly
+                 .GetCallingAssembly()
+                 .GetTypes()
+                 .FirstOrDefault(x => x.Name == $"{cmd}Command");
+
+            if (commandType == null)
+            {
+                throw new InvalidOperationException("Invalid command type");
+            }
+            ICommand command = (ICommand)Activator.CreateInstance(commandType);
+            string result = command.Execute(value);
+            return result;
+
+            //Solution with Reflection:
+            //Type type = Type.GetType("CommandPattern.Commands." + cmd + "Command");
+            //ICommand command = Activator.CreateInstance(type) as ICommand;
+            //string result = command.Execute(value);
+            //return result;
+        }
+    }
+}
---
+++ 07.Reflection and Attributes/Reflection and Attributes - Exercise/P01.CommandPattern/Core/Engine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Models
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+           this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+
+                string cmdExecutionResult = this.commandInterpreter.Read(cmd);
+
+                if(cmdExecutionResult == null)
+                {
+                    break;
+                }
+
+                Console.WriteLine(cmdExecutionResult);
+            }
+        }
+    }
+}
