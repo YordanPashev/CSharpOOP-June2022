8a0c0d21ea0a4d7bc9a1c4c3b83901d69f92426a
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-09 11:09:59 +0300

    Adding Solution to P05 and 06

---
+++ 07.Reflection and Attributes/Reflection and Attributes - Lab/P06.CodeTracker/Tracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            MethodInfo[] methods = type.GetMethods((BindingFlags)60);
+
+            foreach (MethodInfo method in methods)
+            {
+                if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
