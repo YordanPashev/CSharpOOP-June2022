e625f7421ead4cf7143736ab47a1cb5f079ad63b
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-08-08 00:27:28 +0300

    Adding All Problems Descriptions and Solution to P01 for Exam 18 April 2022

---
+++ 11.Exam Preparation/Exam - 18 April 2022/Heroes/Models/Heroes/Hero.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+using Heroes.Models.Contracts;
+
+namespace Heroes.Models.Heroes
+{
+    public abstract class Hero : IHero
+    {
+        private string name;
+        private int health;
+        private int armour;
+        private IWeapon weapon;
+
+        protected Hero(string name, int health, int armour)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.Armour = armour;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Hero name cannot be null or empty.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Health
+        {
+            get => this.health;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Hero health cannot be below 0.");
+                }
+
+                this.health = value;
+            }
+        }
+
+        public int Armour
+        {
+            get => this.armour;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Hero armour cannot be below 0.");
+                }
+
+                this.armour = value;
+            }
+        }
+
+        public IWeapon Weapon
+        {
+            get => this.weapon;
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Weapon cannot be null.");
+                }
+
+                this.weapon = value;
+            }
+        }
+
+        public bool IsAlive  => this.Health > 0 ? true
+                                                : false;
+
+        public void AddWeapon(IWeapon weapon) => this.Weapon = weapon;
+
+        public void TakeDamage(int points)
+        {
+            if (this.Armour - points <= 0)
+            {
+                this.Armour = 0;
+                int attackPointLeft = points - this.Armour;
+
+                if (this.Health - attackPointLeft <= 0)
+                {
+                    this.Health = 0;
+                }
+
+                this.Health -= attackPointLeft;
+            }
+
+            else
+            {
+                this.Armour -= points;
+            }
+        }
+    }
+}
---
+++ 11.Exam Preparation/Exam - 18 April 2022/Heroes/Models/Map/Map.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using Heroes.Models.Contracts;
+using Heroes.Models.Heroes;
+
+namespace Heroes.Models.Map
+{
+    public class Map
+    {
+        public string Fight(ICollection<IHero> players)
+        {
+            List<Knight> knights = new List<Knight>();
+            List<Barbarian> barbarians = new List<Barbarian>();
+
+            foreach (IHero hero in players)
+            {
+                if (hero.IsAlive)
+                {
+                    if (hero is Knight knight)
+                    {
+                        knights.Add(knight);
+                    }
+
+                    else if (hero is Barbarian barbarian)
+                    {
+                        barbarians.Add(barbarian);
+                    }
+                }
+            }
+
+            bool isFightOver = false;
+            string result = string.Empty;
+
+            while (!isFightOver)
+            {
+                foreach (Knight knight in knights.Where(k => k.IsAlive))
+                {
+
+                    foreach (Barbarian barbarian in barbarians.Where(b => b.IsAlive))
+                    {
+                        int dmg = knight.Weapon.DoDamage();
+                        barbarian.TakeDamage(dmg);
+                    }
+                }
+
+                if (!barbarians.Any(b => b.IsAlive))
+                {
+                    result = $"The knights took { knights.Where(k => !k.IsAlive).Count() } casualties but won the battle.";
+                    isFightOver = true;
+                    break;
+                }
+
+                foreach (Barbarian barbarian in barbarians.Where(k => k.IsAlive))
+                {
+
+                    foreach (Knight knight in knights.Where(b => b.IsAlive))
+                    {
+                        int dmg = barbarian.Weapon.DoDamage();
+                        knight.TakeDamage(dmg);
+                    }
+                }
+
+                if (!knights.Any(b => b.IsAlive))
+                {
+                    result = $"The barbarians took { barbarians.Where(k => !k.IsAlive).Count() } casualties but won the battle.";
+                    isFightOver = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
